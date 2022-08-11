fd17dd2cba18cb8f931122af8ab71f039815f298
SunBK201 <sunbk201gm@gmail.com>
2022-08-11 15:39:05 +0800

    fix: header

--- components/Header.js
+++ components/Header.js
@@ -67,7 +67,7 @@ const Header = ({ navBarTitle, fullWidth }) => {
         ref={navRef}
       >
         <div className="flex items-center">
-          <li class="block ml-4 text-black dark:text-gray-50 nav"><a href="/">{BLOG.title}</a></li>
+          <li className="block ml-4 text-black dark:text-gray-50 nav"><a href="/">{BLOG.title}</a></li>
           {navBarTitle
             ? (
             <p className="ml-2 font-medium text-day dark:text-night header-name">
